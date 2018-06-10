3312532d29d9c9477fd4a7b14b5261aa8666cb11
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-06-10 20:04:57 +0200

    7. Move method

--- src/main/java/example/Rental.java
+++ src/main/java/example/Rental.java
@@ -22,10 +22,6 @@ public class Rental {
     }
 
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
