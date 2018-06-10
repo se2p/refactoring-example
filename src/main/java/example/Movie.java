e9a2b4108cc2fd2a02eb2d8c316424d4d414d5e2
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-06-10 20:03:40 +0200

    6. Move method

--- src/main/java/example/Movie.java
+++ src/main/java/example/Movie.java
@@ -25,4 +25,25 @@ public class Movie {
         return title;
     }
 
+    double getCharge(int daysRented) {
+        double result = 0;
+        // determine amounts for each line
+
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDREN:
+                result += 1.5;
+                if(daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
