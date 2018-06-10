88923cc2f7e19beba7e27615a507677f03177604
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-06-10 20:12:03 +0200

    9. Move method

--- src/main/java/example/MovieState.java
+++ src/main/java/example/MovieState.java
@@ -2,6 +2,28 @@ package example;
 
 public abstract class MovieState {
     public abstract int getPriceCode();
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        // determine amounts for each line
+
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if(daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
 
 class RegularMovie extends MovieState {
