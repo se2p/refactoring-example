ee0de09dca481cf991bdb4437fc2bde4edd1fa01
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-06-10 19:58:04 +0200

    4. Extract method, move method

--- src/main/java/example/Customer.java
+++ src/main/java/example/Customer.java
@@ -30,11 +30,7 @@ public class Customer {
             double thisAmount = each.getCharge();
 
             // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
@@ -58,11 +54,7 @@ public class Customer {
             double thisAmount = each.getCharge();
 
             // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this rental
             result += "<li>\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "</li>\n";
