cbe63a65de57c8d912574653c95a8c7663f42eab
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-06-10 19:16:48 +0200

    Original files

---
+++ src/main/java/example/Movie.java
@@ -0,0 +1,28 @@
+package example;
+
+public class Movie {
+    public static final int CHILDREN = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    private String title;
+    private int priceCode;
+
+    // regular movie or for children or ...
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        priceCode = arg;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
